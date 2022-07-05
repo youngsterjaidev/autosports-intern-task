1f5b7c34e6716e85a7be110af0463511eab2a0c5
youngsterjaidev <47059807+youngsterjaidev@users.noreply.github.com>
2022-07-05 14:25:25 +0530

    adding button

---
+++ src/components/elements/button.tsx
@@ -0,0 +1,61 @@
+const Button = styled.button`
+  padding: 8px 12px;
+  font-size: ${typeScale.paragraph};
+  border-radius: 2px;
+  min-width: 100px;
+  cursor: pointer;
+  font-family: ${defaultTheme.primaryFont};
+  transition: background-color 0.2s linear, color 0.2s linear,
+    border 0.2s linear;
+
+  &:hover {
+    background-color: ${defaultTheme.primaryHoverColor};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+
+  &:focus {
+    outline: 3px solid ${defaultTheme.primaryHoverColor};
+    outline-offset: 2px;
+  }
+
+  &:active {
+    background-color: ${defaultTheme.primaryActiveColor};
+    border-color: ${defaultTheme.primaryActiveColor};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    background: none;
+    color: ${defaultTheme.disabled};
+    border: none;
+  }
+`;
+
+const PrimaryButton = styled(Button)`
+  background-color: ${defaultTheme.primaryColor};
+  color: ${defaultTheme.textColorOnPrimary};
+  border: 2px solid transparent;
+
+  &:disabled {
+    background-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.textOnDisabled};
+    border-color: ${defaultTheme.disabled};
+  }
+`;
+
+export const SecondaryButton = styled(Button)`
+  border: 2px solid ${defaultTheme.primaryColor};
+  color: ${defaultTheme.primaryColor};
+
+  &:disabled {
+    border: 2px solid ${defaultTheme.disabled};
+  }
+`;
+
+export const TertiaryButton = styled(Button)`
+  border: 2px solid transparent;
+  color: ${defaultTheme.primaryColor};
+`;
+
+export default PrimaryButton;
